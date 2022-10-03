866212cc43a72af48c3272255ddfe7056a9e8ae9
dianag170 <godjadc@dukes.jmu.edu>
2022-10-03 17:55:52 -0400

    Initial commit - K initialized to 3

---
+++ knn_classifier.py
@@ -0,0 +1,48 @@
+from sklearn.model_selection import KFold
+import numpy as np
+import pandas as pd
+from sklearn import metrics
+from numpy.linalg import norm
+from scipy import spatial
+
+
+
+def classify(X_train, X_test, y_train, y_test, k):
+
+  prediction = []
+  accuracy_score = 0
+
+  for i in range(X_test.shape[0]):
+      cosine_distances = {}
+
+      for j in range(X_train.shape[0]):
+
+          A = X_test.iloc[i,:]
+          B = X_train.iloc[j,:]
+
+          cosine = spatial.distance.cosine(A,B)
+          cosine_distances[(i,j)] = 1-cosine
+        
+      cosine_distances_sorted = dict(sorted(cosine_distances.items(), key=lambda item: item[1],reverse=True))
+      closest_k_neighbors = dict(list(cosine_distances_sorted.items())[0: k])
+    #  print("For test point",i,"k closest distances from training points are", closest_k_neighbors)
+
+      closest_k_neighbors_keys = list(closest_k_neighbors.keys())
+
+      predicted_labels = []
+
+      for pair in closest_k_neighbors_keys:
+        training_index = pair[1]
+        label = y_train[training_index]
+        predicted_labels.append(int(label))
+
+      label_sum = sum(predicted_labels)
+    
+      if label_sum < (k/2):
+        prediction.append('0')
+      elif label_sum > (k/2):
+        prediction.append('1')
+   
+  accuracy_score = metrics.accuracy_score(y_test,prediction)
+
+  return accuracy_score
---
+++ model.py
@@ -0,0 +1,73 @@
+from sklearn.model_selection import KFold
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt 
+plt.rc("font", size=14)
+import seaborn as sns
+sns.set(style="white")
+sns.set(style="whitegrid", color_codes=True)
+from sklearn.model_selection import train_test_split
+from imblearn.over_sampling import SMOTE
+from knn_classifier import classify
+
+
+data=pd.read_csv('Occupancy.csv',header=0)
+data=data.dropna()
+#print(data.shape)
+
+data = data.drop(columns=['date'])
+#print(list(data.columns))
+
+data['Occupancy'].value_counts()
+
+#sns.countplot(x='Occupancy',data=data,palette='hls')
+#plt.show()
+
+count_no_sub = len(data[data['Occupancy']==0])
+count_sub = len(data[data['Occupancy']==1])
+pct_of_no_sub = count_no_sub/(count_no_sub+count_sub)
+print("percentage of no occupancy is", pct_of_no_sub*100)
+pct_of_sub = count_sub/(count_no_sub+count_sub)
+print("percentage of occupancy", pct_of_sub*100)
+
+
+X_all_data = data.loc[:, data.columns != 'Occupancy']
+y_all_labels = data.loc[:, data.columns == 'Occupancy']
+
+
+fold_index = 0
+k = 3
+accuracy_scores = []
+
+
+kf = KFold(n_splits=3)
+for train_index, test_index in kf.split(X_all_data):
+    
+    print("FOLD: ", fold_index)
+
+    X_train, X_test = X_all_data.iloc[train_index,:], X_all_data.iloc[test_index,:]
+    y_train, y_test = y_all_labels.iloc[train_index,:], y_all_labels.iloc[test_index,:]
+
+
+    smote = SMOTE(random_state=0)
+
+    #features
+    columns = X_train.columns
+
+    # SMOTE sampling of training data & labels
+    smote_data_X, smote_labels_y = smote.fit_resample(X_train, y_train)
+
+    smote_data_X = pd.DataFrame(data=smote_data_X,columns=columns )
+    smote_labels_y= pd.DataFrame(data=smote_labels_y,columns=['Occupancy'])
+
+    X_os_train=smote_data_X[columns]
+    y_os_train=smote_labels_y['Occupancy']
+
+    score = classify(X_os_train, X_test, y_os_train, y_test, k)
+    print("Accuracy score wiht K = ",k,"for fold",fold_index,"is",score)
+
+    fold_index += 1
+
+### Average accuracy score
+accuracy_scores_avg = sum(accuracy_scores) / len(accuracy_scores) 
+print("Average accuracy score for K = ",k,"is:", accuracy_scores_avg)
